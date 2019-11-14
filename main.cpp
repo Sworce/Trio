775f6762da324ef35f2802a8faad78e3cd6be10c
Sworce <Sworce>
2019-11-13 16:26:36 -0800

    added function for sorting 3 integers from greatest to least

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
+void rearrange(int&, int&, int&);
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
 
 
   // <-- ADD YOUR FUNCTION CALL HERE
-
+  rearrange(red, green, blue);
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
@@ -23,3 +24,24 @@ int main()
 }
 
 // <-- ADD YOUR FUNCTION DEFINITON HERE
+void rearrange(int& num1, int& num2, int& num3) 
+{   
+    if(num1 < num2)
+    {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    if(num1 < num3)
+    {
+        int temp = num1;
+        num1 = num3;
+        num3 = temp;
+    }
+    if(num2 < num3)
+    {
+        int temp = num2;
+        num2 = num3;
+        num3 = temp;
+    }
+}
